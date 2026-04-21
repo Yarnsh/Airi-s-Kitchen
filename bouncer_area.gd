e95ac23328fd52286026068d32ce9189f4effe79
Yarnsh <yarnsh@gmail.com>
2026-04-21 19:17:44 -0400

    fix bouncer inconsistency

--- bouncer_area.gd
+++ bouncer_area.gd
@@ -1,5 +1,12 @@
 extends Area2D
 
-func _on_body_entered(body: Node2D) -> void:
+func bounce(body):
 	var dir = body.global_position - global_position
 	body.bounce(dir.normalized())
+
+func _on_body_entered(body: Node2D) -> void:
+	bounce(body)
+
+func _physics_process(delta: float) -> void:
+	for b in get_overlapping_bodies():
+		bounce(b)
