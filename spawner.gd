9ce922eb9a5d3147661f1a404108fd9194466294
Yarnsh <yarnsh@gmail.com>
2026-05-11 22:23:14 -0400

    basic setup for throw patterns

--- spawner.gd
+++ spawner.gd
@@ -6,22 +6,65 @@ extends Node2D
 	load("res://FoodObjects/soda.tscn")
 ]
 
-var spawn_delay = 2000
-var next_spawn = 0
+@onready var food_pattern = {
+	"0": {
+		"spawnables": [
+			{
+				"obj": load("res://FoodObjects/beans.tscn"),
+				"time": 2000
+			},
+			{
+				"obj": load("res://FoodObjects/pizza.tscn"),
+				"time": 3000
+			},
+		]
+	},
+	"10000": {
+		"spawnables": [
+			{
+				"obj": load("res://FoodObjects/soda.tscn"),
+				"time": 500
+			}
+		]
+	}
+}
+
+var next_pick_time = 0
+var start_time = 0
+var reverse_pattern_keys = []
 
 @onready var throw_sfx = $ThrowSFX
 @onready var customers = $"../Customers"
 @onready var food_parent = $"../Foods"
 
+func reverse_int_str_sort(a, b):
+	if a.to_int() > b.to_int():
+		return true
+	return false
+
+func pick_food():
+	var now = Time.get_ticks_msec()
+	for t in reverse_pattern_keys:
+		if now - start_time >= t.to_int():
+			var s = food_pattern.get(t, {}).get("spawnables", [null])
+			print(s[randi_range(0, len(s) - 1)]["obj"])
+			return s[randi_range(0, len(s) - 1)]
+	
+	return null
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	next_spawn = Time.get_ticks_msec() + 1000
+	start_time = Time.get_ticks_msec()
+	reverse_pattern_keys = food_pattern.keys()
+	reverse_pattern_keys.sort_custom(reverse_int_str_sort)
 
 func _physics_process(delta: float) -> void:
 	var now = Time.get_ticks_msec()
-	if now >= next_spawn:
-		var spawn_id = randi_range(0, len(spawnables) - 1)
-		var obj = spawnables[spawn_id].instantiate()
+	if now >= next_pick_time:
+		var def = pick_food()
+		next_pick_time = now + def.get("time", 2000)
+		await get_tree().create_timer(float(def.get("time", 2000))/1000.0).timeout
+		var obj = def.get("obj").instantiate() # TODO: default here
 		food_parent.add_child(obj)
 		obj.global_position = global_position
 		obj.velocity = Vector2.RIGHT.rotated(deg_to_rad(-obj.start_angle)) * obj.start_speed
@@ -30,4 +73,3 @@ func _physics_process(delta: float) -> void:
 			Static.game.spare_customers -= 1
 		else:
 			customers.spawn_customer()
-		next_spawn += spawn_delay
