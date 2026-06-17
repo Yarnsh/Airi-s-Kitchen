5f9a5369887d34dd8ca1eb5cfe637beb1bbe649a
Yarnsh <yarnsh@gmail.com>
2026-06-16 22:36:48 -0400

    record bests

--- game.gd
+++ game.gd
@@ -29,21 +29,24 @@ func get_played_time():
 		et = Time.get_ticks_msec()
 	return et - start_time
 func get_played_time_string():
-	var t = get_played_time()
+	return played_time_to_string(get_played_time())
+func played_time_to_string(t):
 	var h = t / 3600000
 	t -= h * 3600000
 	var m = t / 60000
 	t -= m * 60000
 	var s = t / 1000
+	t -= s * 1000
 	
 	var result = ""
 	if h > 0:
 		result += str(h)+":"
 	result += str(m)+":"
-	var ss = str(s)
-	if len(ss) < 2:
-		ss = "0" + ss
-	result += ss
+	var ss = str(s).pad_zeros(2)
+	result += ss+"."
+	var ms = str(t).pad_zeros(3)
+	result += ms
+	
 	return result
 
 func take_life():
@@ -54,5 +57,38 @@ func take_life():
 		if !game_is_over:
 			game_is_over = true
 			end_time = Time.get_ticks_msec()
+			update_records()
 			Static.customers.game_over()
 			Static.game_over_screen.game_over()
+
+func update_records():
+	var records = read_records_from_file()
+	var current = {
+		"score": score,
+		"served": served,
+		"time": get_played_time(),
+		"recorded": Time.get_unix_time_from_system()
+	}
+	if current.score > records.get("best_score", {}).get("score", -1):
+		records["best_score"] = current
+	if current.score > records.get("best_served", {}).get("served", -1):
+		records["best_served"] = current
+	if current.score > records.get("best_time", {}).get("time", -1):
+		records["best_time"] = current
+	
+	write_records_to_file(records)
+
+func write_records_to_file(records):
+	var text = JSON.stringify(records)
+	var file = FileAccess.open("user://records.dat", FileAccess.WRITE)
+	file.store_string(text)
+
+func read_records_from_file():
+	var file = FileAccess.open("user://records.dat", FileAccess.READ)
+	if file == null:
+		return {}
+	var content = file.get_as_text()
+	var result = JSON.parse_string(content)
+	if result == null:
+		return {}
+	return result
